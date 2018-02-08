284afaf783c4073653e1f9833a3805efe4ce4877
Alexey Grachov <grachov.alexey@gmail.com>
2018-02-08 23:55:38 +0200

    Add youtube helper.

--- site/preprocessor.go
+++ site/preprocessor.go
@@ -1,6 +1,8 @@
 package site
 
 import (
+	"fmt"
+
 	"github.com/aymerick/raymond"
 )
 
@@ -12,6 +14,12 @@ func (p *preprocessor) link(page string) string {
 	return p.baseURL + page
 }
 
+func (p *preprocessor) youtube(id string) raymond.SafeString {
+	fmt.Println(id)
+	s := `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/%s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`
+	return raymond.SafeString(fmt.Sprintf(s, id))
+}
+
 func (p *preprocessor) preprocess(content []byte) ([]byte, error) {
 	tmpl, err := raymond.Parse(string(content))
 	if err != nil {
@@ -19,6 +27,7 @@ func (p *preprocessor) preprocess(content []byte) ([]byte, error) {
 	}
 
 	tmpl.RegisterHelper("link", p.link)
+	tmpl.RegisterHelper("youtube", p.youtube)
 
 	res, err := tmpl.Exec(nil)
 	if err != nil {
